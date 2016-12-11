3e39fad9e9df61eedb5db50d75405e7e7c992b4a
Jaha96 <j_jaha28@yahoo.com>
2016-12-11 18:04:18 +0800

    lksdjfhadsf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,8 @@
                     <div class="swiper-slide dark" style="background-image: url('assets/images/slider/swiper/1.jpg');">
                         <div class="container clearfix">
                             <div class="slider-caption slider-caption-center">
-                                <h2 data-caption-animate="fadeInUp">Welcome to Canvas</h2>
-                                <p data-caption-animate="fadeInUp" data-caption-delay="200">Create just what you need for your Perfect Website. Choose from a wide range of Elements &amp; simply put them on your own Canvas.</p>
+                                <h2 data-caption-animate="fadeInUp">Welcome to Mongolia</h2>
+                                
                             </div>
                         </div>
                     </div>
@@ -365,27 +365,7 @@ Sleeping in a rough-and-ready Mongolian ger is a quintessential grassland experi
                     </div>
                 </a>
 
-                <div class="container clearfix">
-
-                    <div class="col_one_third bottommargin-sm center">
-                        <img data-animate="fadeInLeft" src="assets/images/services/iphone6.png" alt="Iphone">
-                    </div>
-
-                    <div class="col_two_third bottommargin-sm col_last">
-
-                        <div class="heading-block topmargin-sm">
-                            <h3>Optimized for Mobile &amp; Touch Enabled Devices.</h3>
-                        </div>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quod consequuntur quibusdam, enim expedita sed quia nesciunt incidunt accusamus necessitatibus modi adipisci officia libero accusantium esse hic, obcaecati, ullam, laboriosam!</p>
-
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti vero, animi suscipit id facere officia. Aspernatur, quo, quos nisi dolorum aperiam fugiat deserunt velit rerum laudantium cum magnam.</p>
-
-                        <a href="#" class="button button-border button-dark button-rounded button-large noleftmargin topmargin-sm">Learn more</a>
-
-                    </div>
-
-                </div>
+              
 
                 <div class="section parallax dark nobottommargin" style="background-image: url('assets/images/services/home-testi-bg.jpg'); padding: 100px 0;" data-stellar-background-ratio="0.4">
 
