974e0ba14b4d4adaf243c511d0c90451bb0c53f0
itsoftmn <fondnessfear@gmail.com>
2016-12-11 17:59:40 +0800

    Revert "latest change"

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -14,11 +14,17 @@
 
                                 <img src="assets/images/footer-widget-logo.png" alt="" class="footer-logo">
 
+                                <p>We believe in <strong>Simple</strong>, <strong>Creative</strong> &amp; <strong>Flexible</strong> Design Standards.</p>
+
                                 <div style="background: url('assets/images/world-map.png') no-repeat center center; background-size: 100%;">
-                                
-                                    <abbr title="Phone Number"><strong>Phone:</strong></abbr>7777-6369<br>
-                                    
-                                    <abbr title="Email Address"><strong>Email:</strong></abbr> info@MongolianGuide.com
+                                    <address>
+                                        <strong>Headquarters:</strong><br>
+                                        795 Folsom Ave, Suite 600<br>
+                                        San Francisco, CA 94107<br>
+                                    </address>
+                                    <abbr title="Phone Number"><strong>Phone:</strong></abbr> (91) 8547 632521<br>
+                                    <abbr title="Fax"><strong>Fax:</strong></abbr> (91) 11 4752 1433<br>
+                                    <abbr title="Email Address"><strong>Email:</strong></abbr> info@canvas.com
                                 </div>
 
                             </div>
@@ -35,7 +41,10 @@
                                     <li><a href="http://codex.wordpress.org/">Documentation</a></li>
                                     <li><a href="http://wordpress.org/support/forum/requests-and-feedback">Feedback</a></li>
                                     <li><a href="http://wordpress.org/extend/plugins/">Plugins</a></li>
-                                    
+                                    <li><a href="http://wordpress.org/support/">Support Forums</a></li>
+                                    <li><a href="http://wordpress.org/extend/themes/">Themes</a></li>
+                                    <li><a href="http://wordpress.org/news/">WordPress Blog</a></li>
+                                    <li><a href="http://planet.wordpress.org/">WordPress Planet</a></li>
                                 </ul>
 
                             </div>
@@ -51,7 +60,29 @@
                                     <div class="spost clearfix">
                                         <div class="entry-c">
                                             <div class="entry-title">
-                                                <h4><a href="#"></a></h4>
+                                                <h4><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h4>
+                                            </div>
+                                            <ul class="entry-meta">
+                                                <li>10th July 2014</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="spost clearfix">
+                                        <div class="entry-c">
+                                            <div class="entry-title">
+                                                <h4><a href="#">Elit Assumenda vel amet dolorum quasi</a></h4>
+                                            </div>
+                                            <ul class="entry-meta">
+                                                <li>10th July 2014</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="spost clearfix">
+                                        <div class="entry-c">
+                                            <div class="entry-title">
+                                                <h4><a href="#">Debitis nihil placeat, illum est nisi</a></h4>
                                             </div>
                                             <ul class="entry-meta">
                                                 <li>10th July 2014</li>
@@ -67,7 +98,24 @@
 
                     <div class="col_one_third col_last">
 
-                    
+                        <div class="widget clearfix" style="margin-bottom: -20px;">
+
+                            <div class="row">
+
+                                <div class="col-md-6 bottommargin-sm">
+                                    <div class="counter counter-small"><span data-from="50" data-to="15065421" data-refresh-interval="80" data-speed="3000" data-comma="true"></span></div>
+                                    <h5 class="nobottommargin">Total Downloads</h5>
+                                </div>
+
+                                <div class="col-md-6 bottommargin-sm">
+                                    <div class="counter counter-small"><span data-from="100" data-to="18465" data-refresh-interval="50" data-speed="2000" data-comma="true"></span></div>
+                                    <h5 class="nobottommargin">Clients</h5>
+                                </div>
+
+                            </div>
+
+                        </div>
+
                         <div class="widget subscribe-widget clearfix">
                             <h5><strong>Subscribe</strong> to Our Newsletter to get Important News, Amazing Offers &amp; Inside Scoops:</h5>
                             <div id="widget-subscribe-form-result" data-notify-type="success" data-notify-msg=""></div>
@@ -98,6 +146,29 @@
                             </script>
                         </div>
 
+                        <div class="widget clearfix" style="margin-bottom: -20px;">
+
+                            <div class="row">
+
+                                <div class="col-md-6 clearfix bottommargin-sm">
+                                    <a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+                                        <i class="icon-facebook"></i>
+                                        <i class="icon-facebook"></i>
+                                    </a>
+                                    <a href="#"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+                                </div>
+                                <div class="col-md-6 clearfix">
+                                    <a href="#" class="social-icon si-dark si-colored si-rss nobottommargin" style="margin-right: 10px;">
+                                        <i class="icon-rss"></i>
+                                        <i class="icon-rss"></i>
+                                    </a>
+                                    <a href="#"><small style="display: block; margin-top: 3px;"><strong>Subscribe</strong><br>to RSS Feeds</small></a>
+                                </div>
+
+                            </div>
+
+                        </div>
+
                     </div>
 
                 </div><!-- .footer-widgets-wrap end -->
@@ -111,8 +182,8 @@
                 <div class="container clearfix">
 
                     <div class="col_half">
-                        Copyrights &copy; 2016 All Rights Reserved by MongolianGuide inc.<br>
-                        
+                        Copyrights &copy; 2014 All Rights Reserved by Canvas Inc.<br>
+                        <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
                     </div>
 
                     <div class="col_half col_last tright">
@@ -160,7 +231,7 @@
 
                         <div class="clear"></div>
 
-                        <i class="icon-envelope2"></i> info@MongolianGuide.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> +7777-6369 <span class="middot">&middot;</span> <i class="icon-skype2"></i>
+                        <i class="icon-envelope2"></i> info@canvas.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> +91-11-6541-6369 <span class="middot">&middot;</span> <i class="icon-skype2"></i> CanvasOnSkype
                     </div>
 
                 </div>
